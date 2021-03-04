9b5f0ea9fbe646d957dd5bb273cf11565008f9a5
Kellen_May <kellenbmay@gmail.com>
2021-03-04 16:03:58 -0500

    commit tree 69476951d347ef0854012f9c73749b8bef7f8422

--- db/migrate/20210222211122_create_recipes.rb
+++ db/migrate/20210222211122_create_recipes.rb
@@ -1,9 +1,9 @@
-class CreateRecipes < ActiveRecord::Migration
+class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
-      t.string :name
-      t.string :ingredients
-      t.string :cook_time
+      t.string :recipe_name
+      t.string :recipe_ingredients
+      t.string :Recipe_cook_time
     end
   end
 end
